8a4858147634361f30e2629299781f99bf5722d5
lagmanzaza <lagmanmc@gmail.com>
2021-08-04 14:29:57 +0700

    chore: log

--- backend/src/utility/transform-enum.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.transformEnumValueToArray = void 0;
-function transformEnumValueToArray(data) {
-    return Object.keys(data).map(function (value) { return value; });
-}
-exports.transformEnumValueToArray = transformEnumValueToArray;
